acc721af280022f6e2a6c3957764411e81fb52b6
Jake Walsh <jkwalsh127@gmail.com>
2022-05-31 17:26:19 -0700

    write validations for form inputs

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -9,6 +9,9 @@ export default function Form() {
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [errorNameMessage, setErrorNameMessage] = useState('');
+    const [errorEmailMessage, setErrorEmailMessage] = useState('');
+    const [errorMessageMessage, setErrorMessageMessage] = useState('');
 
     // Get name and value of input that triggered the change
     const handleInputChange = (e) => {
@@ -25,6 +28,37 @@ export default function Form() {
             setMessage(inputValue);
         }
     };
+
+    const handleNameBlur = () => {
+        if (!userName) {
+            setErrorNameMessage('Please include your name');
+            return;
+        }
+        if (userName) {
+            setErrorNameMessage('');
+            return;
+        }
+    }
+    const handleEmailBlur = () => {
+        if (!userName) {
+            setErrorEmailMessage('Please enter a valid email');
+            return;
+        }
+        if (userName) {
+            setErrorEmailMessage('');
+            return;
+        }
+    }
+    const handleMessageBlur = () => {
+        if (!userName) {
+            setErrorMessageMessage('Please include a message');
+            return;
+        }
+        if (userName) {
+            setErrorMessageMessage('');
+            return;
+        }
+    }
 
     const handleFormSubmit = (e) => {
         // Prevent page refresh
@@ -53,35 +87,52 @@ export default function Form() {
     return (
         <div className='form-wrapper'>
             <h3>Send me a message</h3>
-            <form>
-                <h4>Your Name:</h4>
+            <form className='message-form'>
+                <h4 className='field-label'>Your Name:</h4>
+                {errorNameMessage && (
+                <div>
+                    <p>{errorNameMessage}</p>
+                </div>
+                )}
                 <input
                     className='field'
                     value={userName}
                     name='userName'
-                    onBlur={handleInputChange}
+                    onChange={handleInputChange}
+                    onBlur={handleNameBlur}
                     type='text'
                     placeholder='Your Name'
                 />
-                <h4>Email address:</h4>
+                <h4 className='field-label'>Email address:</h4>
+                {errorEmailMessage && (
+                <div>
+                    <p>{errorEmailMessage}</p>
+                </div>
+                )}
                 <input
                     className='field'
                     value={email}
                     name='email'
-                    onBlur={handleInputChange}
+                    onChange={handleInputChange}
+                    onBlur={handleEmailBlur}
                     type='email'
                     placeholder='Your Email'
                 />
-                <h4>Message:</h4>
-                <input
+                <h4 className='field-label'>Message:</h4>
+                {errorMessageMessage && (
+                <div>
+                    <p>{errorMessageMessage}</p>
+                </div>
+                )}
+                <textarea
                     className='message-field field'
                     value={message}
                     name='message'
-                    onBlur={handleInputChange}
+                    onChange={handleInputChange}
+                    onBlur={handleMessageBlur}
                     type='text'
-                    placeholder='Leave me a message'
                 />
-                <button type='button' onClick={handleFormSubmit}>Submit</button>
+                <button className='submit-btn' type='button' onClick={handleFormSubmit}>Submit</button>
             </form>
             {errorMessage && (
                 <div>
